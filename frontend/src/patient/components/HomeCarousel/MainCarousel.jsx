82e226da8a19625e1c09d7ffdcaa42348a2c1b2b
Nikhil Deshmukh <115735153+nikhildeshmukh170@users.noreply.github.com>
2024-08-31 02:36:07 +0530

    add navbar and carousel

---
+++ frontend/src/patient/components/HomeCarousel/MainCarousel.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import { MainCaroselData } from './MainCaroselData';
+
+const maincarosel = () => {
+
+    const items = MainCaroselData.map((item, index) => (
+        <div key={index} className='carousel-item h-[20rem] object-contain'>
+            <img className='cursor-pointer w-full' role='presentation' src={item.src} alt={`Carousel item ${index}`} />
+        </div>
+    ));
+
+  return (
+    <div>
+        <AliceCarousel
+        // mouseTracking
+        items={items}
+        disableButtonsControls
+        autoPlay
+        autoPlayInterval={2000}
+        infinite
+    />
+    </div>
+  )
+}
+
+export default maincarosel
