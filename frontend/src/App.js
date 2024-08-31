9e8b20f132af395f14566a42b26e89c81bf838d1
Nikhil Deshmukh <115735153+nikhildeshmukh170@users.noreply.github.com>
2024-08-31 18:52:51 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './patient/pages/Home/Home';
 import LoginPage from './patient/components/Login/Login';
+import Home from './patient/pages/Home/Home';
+import DoctorHome from './patient/pages/Home/DoctorHome/DoctorHome';
 
 function App() {
+  // Assume userRole is determined after login
+  // It could be 'patient' or 'doctor'
+  const [userRole, setUserRole] = React.useState(null);
+
   return (
     <Router>
-      <div className='App'>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/Login" element={<LoginPage />}></Route>
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/login" element={<LoginPage setUserRole={setUserRole} />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/doctor-home" element={<DoctorHome />} />
+        {/* Redirect to appropriate home page based on role */}
+        <Route
+          path="/"
+          element={
+            userRole === 'patient' ? (
+              <Navigate to="/home" />
+            ) : userRole === 'doctor' ? (
+              <Navigate to="/doctor-home" />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
+      </Routes>
     </Router>
-  )
+  );
 }
 
 export default App;
---
+++ frontend/src/patient/pages/Home/DoctorHome/DoctorHome.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DoctorHome = () => {
+  return (
+    <div>DoctorHome</div>
+  )
+}
+
+export default DoctorHome
