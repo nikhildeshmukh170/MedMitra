7a9969c9de6cd1429943ff2d645f4ce9bc4401c8
Nikhil Deshmukh <115735153+nikhildeshmukh170@users.noreply.github.com>
2024-08-31 17:58:10 +0530

    complete patient page

--- frontend/src/patient/components/HospitalDetails/HospitalDetails.jsx
+++ frontend/src/patient/components/HospitalDetails/HospitalDetails.jsx
@@ -1,15 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import HospitalItemCard from '../HospitalItemCard/HospitalItemCard';
-import hospitalData from './HospitalData'; // Import the data
 
-const HospitalDetails = () => {
-  // Use imported data as the initial state
-  const [item_list, setItemList] = useState(hospitalData);
-
+const HospitalDetails = ({ hospitalData }) => {
   return (
-    <div className="HospitalDetails flex flex-col  overflow-x-auto h-[36rem] w-[90rem] gap-8 m-auto">
-      <div className="Hospital_item flex flex-col  h-auto w-[80rem] m-auto gap-4">
-        {item_list.map((item, index) => (
+    <div className="HospitalDetails flex flex-col overflow-x-auto h-[36rem] w-[90rem] gap-8 m-auto">
+      <div className="Hospital_item flex flex-col h-auto w-[80rem] m-auto gap-4">
+        {hospitalData.map((item, index) => (
           <HospitalItemCard
             key={index}
             id={item._id}
