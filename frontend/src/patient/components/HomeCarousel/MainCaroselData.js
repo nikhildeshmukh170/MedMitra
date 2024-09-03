7124c2554ffb8e5d753b322c06a0f03aa37fd07e
Nikhil Deshmukh <115735153+nikhildeshmukh170@users.noreply.github.com>
2024-09-04 01:50:03 +0530

    update check

--- frontend/src/patient/components/HomeCarousel/MainCaroselData.js
+++ frontend/src/patient/components/HomeCarousel/MainCaroselData.js
@@ -1,35 +1,35 @@
-// import medmitracarousel6 from "../../../assets/medmitracarousel1.png"
-// import medmitracarousel1 from "../../../assets/medmitracarousel1.png"
-import medmitracarousel2 from "../../../assets/medmitracarousel2.png"
-import medmitracarousel3 from "../../../assets/medmitracarousel3.png"
-import medmitracarousel4 from "../../../assets/medmitracarousel4.png"
-import medmitracarousel5 from "../../../assets/medmitracarousel5.png"
+// // import medmitracarousel6 from "../../../assets/medmitracarousel1.png"
+// // import medmitracarousel1 from "../../../assets/medmitracarousel1.png"
+// import medmitracarousel2 from "../../../assets/medmitracarousel2.png"
+// import medmitracarousel3 from "../../../assets/medmitracarousel3.png"
+// import medmitracarousel4 from "../../../assets/medmitracarousel4.png"
+// import medmitracarousel5 from "../../../assets/medmitracarousel5.png"
 
-export const MainCaroselData = [
-    // {
-    //     id: "01",
-    //     src: medmitracarousel1
+// export const MainCaroselData = [
+//     // {
+//     //     id: "01",
+//     //     src: medmitracarousel1
 
-    // },
-    {
-        id: "02",
-        src: medmitracarousel2
+//     // },
+//     {
+//         id: "02",
+//         src: medmitracarousel2
 
-    },
-    {
-        id: "03",
-        src: medmitracarousel3
+//     },
+//     {
+//         id: "03",
+//         src: medmitracarousel3
 
-    },
-    {
-        id: "04",
-        src: medmitracarousel4
+//     },
+//     {
+//         id: "04",
+//         src: medmitracarousel4
 
-    },
-    {
-        id: "05",
-        src: medmitracarousel5
+//     },
+//     {
+//         id: "05",
+//         src: medmitracarousel5
 
-    },
+//     },
 
-]
+// ]
