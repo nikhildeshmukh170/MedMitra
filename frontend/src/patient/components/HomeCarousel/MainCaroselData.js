f614915ce111bcc3011aeb48b48757225f59b8f8
Nikhil Deshmukh <115735153+nikhildeshmukh170@users.noreply.github.com>
2024-09-10 18:36:11 +0530

    carousel bug solve

--- frontend/src/patient/components/HomeCarousel/MainCaroselData.js
+++ frontend/src/patient/components/HomeCarousel/MainCaroselData.js
@@ -1,16 +1,16 @@
 // import medmitracarousel6 from "../../../assets/medmitracarousel1.png"
-// import medmitracarousel1 from "../../../assets/medmitracarousel1.png"
+import medmitracarousel1 from "../../../assets/medmitracarousel1.png"
 import medmitracarousel2 from "../../../assets/medmitracarousel2.png"
 import medmitracarousel3 from "../../../assets/medmitracarousel3.png"
 import medmitracarousel4 from "../../../assets/medmitracarousel4.png"
 import medmitracarousel5 from "../../../assets/medmitracarousel5.png"
 
 export const MainCaroselData = [
-    // {
-    //     id: "01",
-    //     src: medmitracarousel1
+    {
+        id: "01",
+        src: medmitracarousel1
 
-    // },
+    },
     {
         id: "02",
         src: medmitracarousel2
